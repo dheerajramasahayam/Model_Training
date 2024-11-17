222c26f30e787720c9e4fe3c4851f4eea512d373
Dheeraj Ramasahayam <dheerajramasahayam@gmail.com>
2024-11-17 11:54:43 -0500

    MInor changes

--- src/data_generator.py
+++ src/data_generator.py
@@ -49,7 +49,7 @@ def generate_handshake(ssid, bssid, encryption_type="WPA2-PSK"):
     client_mac = RandMAC()
 
     # Add some fuzzing to the packets (random variations)
-    asso_req = fuzz(Dot11AssoReq(cap='ESS+privacy', addr1=bssid, addr2=client_mac, addr3=bssid))
+    asso_req = fuzz(Dot11AssoReq(cap='ESS+privacy', addr2=bssid, addr1=client_mac, addr3=bssid))
     asso_resp = fuzz(Dot11AssoResp(cap='ESS+privacy', addr1=client_mac, addr2=bssid, addr3=bssid))
     auth_req = fuzz(Dot11Auth(algo=0, seqnum=1, status=0, addr1=bssid, addr2=client_mac, addr3=bssid))
     auth_resp = fuzz(Dot11Auth(algo=0, seqnum=2, status=0, addr1=client_mac, addr2=bssid, addr3=bssid))
